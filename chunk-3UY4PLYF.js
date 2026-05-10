74a03e320f300bbcf4c03742bf5c03a6abca0a66
roaa676 <roaaezzat676@gmail.com>
2026-05-10 15:03:28 +0300

    Auto-generated commit

---
+++ chunk-3UY4PLYF.js
@@ -0,0 +1,1 @@
+import"./chunk-YEWEO3AL.js";var t=[{path:"",pathMatch:"full",redirectTo:"home"},{path:"home",loadComponent:()=>import("./chunk-MPF4I2U3.js").then(o=>o.HomeComponent)}];export{t as default};
--- chunk-PTCP562L.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-YEWEO3AL.js";var t=[{path:"",pathMatch:"full",redirectTo:"home"},{path:"home",loadComponent:()=>import("./chunk-VO2QE23T.js").then(o=>o.HomeComponent)}];export{t as default};
